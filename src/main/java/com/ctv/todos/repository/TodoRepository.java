7f6dd533399bbd9f1f05e316567161562f37648b
Macon Pegram <mpegram@captechventures.com>
2015-06-01 06:21:40 -0400

    Changed signature to return Iterable

--- src/main/java/com/ctv/todos/repository/TodoRepository.java
+++ src/main/java/com/ctv/todos/repository/TodoRepository.java
@@ -1,13 +1,11 @@
 package com.ctv.todos.repository;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 import com.ctv.todos.model.Todo;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 
-	List<Todo> findByDoneTrue();	
-	List<Todo> findByDoneFalse();
+	Iterable<Todo> findByDoneTrue();	
+	Iterable<Todo> findByDoneFalse();
 }
