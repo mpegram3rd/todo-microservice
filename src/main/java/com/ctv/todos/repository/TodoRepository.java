529e744add7ff563038e2735a10b3baf545dd862
Macon Pegram <mpegram@captechventures.com>
2015-06-01 05:30:58 -0400

    Final project starter bits

---
+++ src/main/java/com/ctv/todos/repository/TodoRepository.java
@@ -0,0 +1,12 @@
+package com.ctv.todos.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ctv.todos.model.Todo;
+
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+	List<Todo> findByDoneTrue();
+}
